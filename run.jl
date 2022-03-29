180f4f851a70057cf9ba6ddef53a951f172275a2
Thomas Guignard <tom@timtom.ch>
2022-03-29 12:33:07 -0400

    Tested code with own image

--- run.jl
+++ run.jl
@@ -3,5 +3,5 @@ Pkg.activate(".")
 
 using Images, CausticsEngineering
 
-image = Images.load("./examples/cat_posing.jpg") # Check current working directory with pwd()
+image = Images.load("./input/logos_patente512x512.jpg") # Check current working directory with pwd()
 engineer_caustics(image);
